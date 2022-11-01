6fc104202c2e445a02ec48dd2d01f8aa11521e6d
Aditya Srichandan <adityasrichandan3098@gmail.com>
2022-11-01 09:03:07 +0530

    Trader_v1
    Developed code for EMA and instant trend definitions

--- Trader.py
+++ Trader.py
@@ -1,6 +1,11 @@
 #encoding utf-8
 import logging as lg
-import sys
+import sys, time, os, pytz
+import pandas as pd
+import datetime as datetime
+from math import ceil
+import alpaca_trade_api as tradeapi
+
 class Trader(object):
     def __init__(self, ticker):
         self.ticker = ticker
@@ -71,5 +76,86 @@ class Trader(object):
             lg.error('The position {} is incorrect'.format(position))
             sys.exit()
         return takeProfit
+    
+    def get_open_position(self, assetId):
+        postitions = None
+        for position in positions:
+            if position.symbol == assetId:
+                return True
+            else:
+                return False
+    
+    def check_position(self,asset):
+        attempt = 1
+        maxAttempt = 10
+        while attempt <= maxAttempt:
+            try:
+                curr_price = position.current_price
+                lg.info('The position was checked. Current price :- %.4f' % curr_price)
+                return True
+            except:
+                lg.info('The position not found, searching... attempt {}'.format(attempt))
+                time.sleep(5)
+                attempt+=1
+        lg.info('No positions found for asset {}'.format(asset))
+        return False
+    
+    def get_overall_trend(self,asset):
+        attempts = 1
+        maxAttempt = 10
+        waitTime = 60
+        try:
+            while True:
+                ema_shortest = None
+                ema_short = None
+                ema_long = None
+                lg.info('% overall trend EMAs : [%.2f,%.2f,%.2f]'%(asset,ema_shortest,ema_short,ema_long))
+                if condition:
+                    lg.info('Trend detected for asset {}: Upward'.format(asset))
+                    return 'long'
+                elif condition2:
+                    lg.info('Trend detected for asset {}: Downward'.format(asset))
+                    return short
+                elif attempts<=maxAttempt:
+                    lg.info('Overall Trend not clear, waiting...')
+                    attempts+=1
+                    time.sleep(waitTime*maxAttempt)
+                else:
+                    lg.info('No Trend detected & Timeout')
+                    return 'no trend'
+        except Exception as e:
+            lg.error('Something went wrong to get overall trend')
+            lg.error(e)
+            sys.exit()
+
+    def get_instant_trend(self,asset,overall_trend):
+        attempts = 1
+        maxAttempt = 10
+        waitTime = 30
+        try:
+            while True:
+                #get data
+                ema_shortest = None
+                ema_short = None
+                ema_long = None
+                lg.info('% instant trend EMAs : [%.2f,%.2f,%.2f]'%(asset,ema_shortest,ema_short,ema_long))
+                if overall_trend == 'long' and (ema_shortest>ema_short) and (ema_short>ema_long):
+                    lg.info('Long instant trend Confirmed for asset {}'.format(asset))
+                    return True
+                elif overall_trend == 'short' and (ema_shortest<ema_short) and (ema_short<ema_long):
+                    lg.info('Short instant trend detected for asset {}'.format(asset))
+                    return True 
+                elif attempts<=maxAttempt:
+                    lg.info('Instant Trend not clear, waiting...')
+                    attempts+=1
+                    time.sleep(waitTime*maxAttempt)
+                else:
+                    lg.info('No instant trend detected & Timeout')
+                    return 'no trend'
+        except Exception as e:
+            lg.error('Something went wrong to get instant trend')
+            lg.error(e)
+            sys.exit()
+
     def run_trade():
         pass
