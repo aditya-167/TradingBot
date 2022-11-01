615e13327fdb88a8e0dc3912cb7545943195f0c5
Aditya Srichandan <adityasrichandan3098@gmail.com>
2022-11-01 07:36:46 +0530

    logger_v0
    initialize logger

--- logger.py
+++ logger.py
@@ -1,4 +1,22 @@
 import logging as lg
+import os
+from datetime import datetime 
 
-filename = None
-lg.basicConfig(filename=filename, format = '%(asctime)s - %(levelname)s: %(message)s',leve)
+def initialize_logging():
+    log_dir = 'logs/'
+
+    try:
+        os.mkdir(log_dir)
+    except OSError:
+        print('Failed to create directory at {}'.format(log_dir))
+    else:
+        print("Succsessfuly created directory at {}".format(log_dir))
+
+    date = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_name = date + '.log'
+    curr_log_path = log_dir + log_name
+    lg.basicConfig(filename=curr_log_path, format = '%(asctime)s - %(levelname)s: %(message)s',level = lg.DEBUG)
+
+    lg.getLogger().addHandler(lg.StreamHandler())
+
+    lg.info('Log Initialized')
